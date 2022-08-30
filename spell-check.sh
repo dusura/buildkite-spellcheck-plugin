0a6a5b6f24aba263301306546ec12076eed243e4
David Donn <David.Donn@team.telstra.com>
2022-08-30 11:18:40 +1000

    x

--- spell-check.sh
+++ spell-check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker run --rm -ti -v $(pwd):/workdir tmaier/markdown-spellcheck:latest --report "**/*.md" | build-agent annotate
+docker run --rm -ti -v $(pwd):/workdir tmaier/markdown-spellcheck:latest --report "**/*.md"
+
+cat markdown-it-sample.md | buildkite-agent annotate
 
 # image exits with 1 when there are errors
 exit 0
