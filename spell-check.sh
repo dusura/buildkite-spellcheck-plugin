a4c8cbb288650cb1dcfd8df062784c800006c9db
David Donn <David.Donn@team.telstra.com>
2022-08-30 11:37:53 +1000

    x

--- spell-check.sh
+++ spell-check.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-docker run --rm -ti -v $(pwd):/workdir tmaier/markdown-spellcheck:latest --report "**/*.md"
-
-cat markdown-it-sample.md | buildkite-agent annotate
+docker run --rm -ti -v $(pwd):/workdir tmaier/markdown-spellcheck:latest --report "**/*.md" \
+  | sed 's/^/```term/'
+  | sed 's/$/```/'
+  | buildkite-agent annotate
 
 # image exits with 1 when there are errors
 exit 0
