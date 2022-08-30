53a7a55583795df21ea431ad57551936ea3db48f
David Donn <David.Donn@team.telstra.com>
2022-08-30 10:15:33 +1000

    x

--- spell-check.sh
+++ spell-check.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker run --rm -ti -v $(pwd):/workdir tmaier/markdown-spellcheck:latest --report "**/*.md"
+
+PATTERN="$BUILDKITE_PLUGIN_SPELL_CHECKER_PATTERN"
+
+docker run --rm -ti -v $(pwd):/workdir tmaier/markdown-spellcheck:latest --report "$PATTERN"
+
+# image exits with 1 when there are errors
+exit 0
