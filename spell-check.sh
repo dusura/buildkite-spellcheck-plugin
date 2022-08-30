fc2fee40ce9d683edc8e7f3f2e53a95f3d1f7c6a
David Donn <David.Donn@team.telstra.com>
2022-08-30 11:40:24 +1000

    x

--- spell-check.sh
+++ spell-check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker run --rm -ti -v $(pwd):/workdir tmaier/markdown-spellcheck:latest --report "**/*.md" \
-  | sed 's/^/```term/'
-  | sed 's/$/```/'
+  | sed '^s/^/```term/'
+  | sed '$s/$/```/'
   | buildkite-agent annotate
 
 # image exits with 1 when there are errors
